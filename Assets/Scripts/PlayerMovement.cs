fad1d661e7afe472f2512e5ed8b8aa8f5db1c0ad
NakharinBU <nakharin.sise@bumail.net>
2024-11-11 14:44:17 +0700

    Add Animation + Fixed Movement
    เพิ่ม Animation และ แก้ไขโค้ด PlayerMovement
    เพิ่ม Asset ใหม่

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb2d;
+    [SerializeField] Animator animator;
+
     public float acceleration;
     public float groundSpeed;
     public float jumpSpeed;
 
-    [Range(0,1f)]
+    [Range(0, 1f)]
     public float groundDecay;
-    public bool grounded;
+    public bool isGrounded;
     public BoxCollider2D groundCheck;
     public LayerMask groundMask;
     float xInput;
@@ -20,6 +22,7 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -33,52 +36,64 @@ public class PlayerMovement : MonoBehaviour
     {
         CheckGround();
         ApplyFriction();
+        animator.SetFloat("xVelocity", Mathf.Abs(rb2d.velocity.x));
+        animator.SetFloat("yVelocity", jumpSpeed);
     }
 
     public void CheckGround()
     {
-        grounded = Physics2D.OverlapAreaAll(groundCheck.bounds.min, groundCheck.bounds.max, groundMask).Length > 0;
+        isGrounded = Physics2D.OverlapAreaAll(groundCheck.bounds.min, groundCheck.bounds.max, groundMask).Length > 0;
     }
 
-    public void GetInput() 
+    public void GetInput()
     {
         xInput = Input.GetAxisRaw("Horizontal");
         yInput = Input.GetAxisRaw("Vertical");
     }
 
-    public void MoveWithInput() 
+    public void MoveWithInput()
     {
         if (Mathf.Abs(xInput) > 0)
-        {
+            {
             float increment = xInput * acceleration;
-            float newSpeed = Mathf.Clamp(rb2d.velocity.x + increment, -groundSpeed , groundSpeed);
-            rb2d.velocity = new Vector2(xInput * groundSpeed, rb2d.velocity.y);
-
+            float newSpeed = Mathf.Clamp(rb2d.velocity.x + increment, -groundSpeed, groundSpeed);
+            rb2d.velocity = new Vector2(newSpeed, rb2d.velocity.y);
             FaceInput();
         }
-    }
-
-    public void HandleJump() 
-    {
-        if (Input.GetButtonDown("Jump") && grounded)
+        else
         {
-            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
+            // หยุดการเคลื่อนที่ในแนวนอนเมื่อไม่ได้กดปุ่ม
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
         }
     }
 
-
-    public void ApplyFriction() 
+    public void HandleJump()
     {
-        if (grounded && xInput == 0 && rb2d.velocity.y <= 0)
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
+            animator.SetBool("isJumping", isGrounded);
+
+        }
+    }
+
+    public void ApplyFriction()
+    {
+        if (isGrounded && xInput == 0 && rb2d.velocity.y <= 0)
         {
             rb2d.velocity *= groundDecay;
         }
     }
 
-    public void FaceInput() 
+    public void FaceInput()
     {
         float direction = Mathf.Sign(xInput);
         transform.localScale = new Vector3(direction, 1, 1);
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+        animator.SetBool("isJumping", !isGrounded);
+    }
 }
